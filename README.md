919f7191f25cb8a9f561ec29400d60eb9ed7f9a9
Madjidgo <madjidgo@gmail.com>
2017-11-12 17:38:28 +0100

    portfolio construct in 2days with Materialize

---
+++ README.md
@@ -0,0 +1,3 @@
+# portfolio2
+Portfolio with new skills
+This new project is that of a 4-month apprenticeship at Simplon Roubaix
---
+++ js/main.js
@@ -0,0 +1,42 @@
+  // jQuery
+  $('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 500);
+    }
+});
+// collapse
+$( document ).ready(function(){
+$(".button-collapse").sideNav();
+});
+
+// caroussel
+$(document).ready(function(){
+    $('.carousel').carousel();
+    setInterval(function() {
+    $('.carousel').carousel('next');
+  }, 2000); // every 2 seconds
+  });
+
+
+// paralax
+$(document).ready(function(){
+     $('.parallax').parallax();
+   });
+
+// scrollSpy
+   $(document).ready(function(){
+     $('.scrollspy').scrollSpy();
+   });
+
+
+    $(document).ready(function(){
+      $('.modal').modal();
+
+ });
